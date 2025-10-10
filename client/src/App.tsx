85a33785f45fd944b6004e64f76b38f741bc599c
Sofia Rodrigues <sofia@algebraic.dev>
2025-10-10 18:38:40 -0300

    feat: mathlib when from=mathlib

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import * as monaco from 'monaco-editor'
 import CodeMirror, { EditorView } from '@uiw/react-codemirror'
 import { LeanMonaco, LeanMonacoEditor, LeanMonacoOptions } from 'lean4monaco'
 import LZString from 'lz-string'
-import NavBarLean, { NavBar, NavItem } from './NavBar'
+import {NavBarLean, NavBar, NavItem, NavBarMathLib } from './NavBar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCode } from '@fortawesome/free-solid-svg-icons'
 import * as path from 'path'
@@ -378,8 +378,17 @@ function App() {
     }
   }, [])
 
+  const [isMathLib, setIsMathLib] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const nameParam = params.get('from'); // e.g., ?name=Sofia
+    setIsMathLib(nameParam == "mathlib")
+  }, []);
+
   return <PreferencesContext.Provider value={{preferences, setPreferences}}>
-    <NavBarLean></NavBarLean>
+    { isMathLib ? <NavBarMathLib /> : <NavBarLean /> }
+    
     <div className="app monaco-editor">
       <nav>
         <Menu
