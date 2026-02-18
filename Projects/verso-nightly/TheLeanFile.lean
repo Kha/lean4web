2c5e51710d13eb65aea224fb682d34f0118b32b3
Robert J. Simmons <442315+robsimmons@users.noreply.github.com>
2026-02-18 16:56:48 -0500

    Work without an executable (#1)
    * Switch away from exe-based html rendering
    
    * remove main, switch to SSE
    
    * Comments, change bubblewrap command
    
    * Cleaning up execution
    
    * Prettier update

--- Projects/verso-nightly/TheLeanFile.lean
+++ Projects/verso-nightly/TheLeanFile.lean
@@ -2,11 +2,10 @@ import VersoManual
 open Verso Doc
 open Verso.Genre Manual
 open Verso.Genre.Manual.InlineLean
+
 #doc (Manual) "My Document" =>
 
-This is a Verso document.
-
-
+This is an example Verso document.
 
 It can include inline math, like this: $`x + 4 = -3`.
 
@@ -17,11 +16,9 @@ We also support inline lean, like this: {lean}`Nat.add_assoc`.
 
 We also support block lean, like this:
 
-dadsf
-
 ```lean
 /-- The name we will be greeting -/
 def theName := "Jimothy"
 
-#eval s!"Hello {theName}"
+#eval s!"Hello {theName} again again"
 ```
