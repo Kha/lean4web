af24924d7d8e535a85f7a326c90f78b05be2dcfe
Rob Simmons <rob@lean-fro.org>
2026-02-19 08:26:27 -0500

    Sample verso doc

--- Projects/verso-nightly/TheLeanFile.lean
+++ Projects/verso-nightly/TheLeanFile.lean
@@ -1,10 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Error</title>
-</head>
-<body>
-<pre>Cannot GET /api/examples/verso-nightly/MakeVerso/VersoExample.lean</pre>
-</body>
-</html>
+import VersoManual
+#doc (Verso.Genre.Manual) "My Document" => Verso doc
